d052fa2b538fa445b996ece4c27b1ba95ed14b94
Anggri <hendrika_ang@apps.ipb.ac.id>
2020-05-19 14:23:27 +0700

    Update register.php

--- register.php
+++ register.php
@@ -11,9 +11,17 @@ if(isset($_POST['register'])){
     $password2 = $_POST['password2'];
 
     if($password1 == $password2){
-        // query
-        $query = pg_query("INSERT INTO nitiperz(nama, email, password, telepon)
-                            VALUES('$nama', '$email', '$password1', '$telepon')");
+        // query tabel nitiperz
+        $query1 = pg_query($db, "INSERT INTO nitiperz(nama, email, password, telepon)
+                            VALUES('$nama', '$email', '$password1', '$telepon');");
+        $ambilNitiperz = pg_query($db, "SELECT * FROM nitiperz WHERE nama=$nama AND email=$email;");
+        $data = pg_fetch_assoc($ambilNitiperz);
+        $userID = $data['userid'];
+        // query tabel mitra
+        $query2 = pg_query($db, "INSERT INTO mitra(userID, jammulai, jamselesai) 
+                            VALUES($userID, '00:00:00', '00:00:00');");
+        // query tabel customer
+        $query3 = pg_query($db, "INSERT INTO customer(userID) VALUES($userID);");
     }else{
         echo("Password dan pengulangannya berbeda");
     }
